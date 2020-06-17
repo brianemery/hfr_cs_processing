9b678a21b6e8b2d69f35392772a8aa6e5a30468f
brianemery <brian.emery@ucsb.edu>
2020-06-16 19:31:21 -0700

    adding installer

---
+++ install_hfr_cs_proc.m
@@ -0,0 +1,32 @@
+% HFR CS PROCESSING - installation script
+%
+% INSTRUCTIONS
+% - download and unzip the toolbox (eg via the clone or download button)
+% - cd into the directory, eg:
+%      cd ~/Downloads/hfr_cs_processing-master/
+% - run this script:
+%     >> install_hfr_cs_proc
+%
+
+% June 2020 Brian Emery
+
+% get the directory locating this file
+wd = fileparts( mfilename('fullpath') );
+
+% list the directories
+flist = dir(fullfile(wd, ['*' filesep '*.*']));
+
+% get just the directories
+flist = flist([flist.isdir]);  
+
+% ... in a cell array
+flist = unique({flist.folder}');
+
+% add these to the path
+disp('Modifying MATLAB Path ...')
+
+addpath(wd,'-end')
+
+for i = 1:numel(flist)
+    addpath(flist{i},'-end')
+end
